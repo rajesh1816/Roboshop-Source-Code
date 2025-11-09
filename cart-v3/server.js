49b3ab4a4c5b5cd64b0843bab8165f27f8314ae9
rajesh <rajeshkaramsetty1999@gmail.com>
2025-11-09 21:54:25 +0530

    adding mongodb service

--- cart-v3/server.js
+++ cart-v3/server.js
@@ -401,7 +401,7 @@ redisClient.on('ready', (r) => {
 });
 
 // fire it up!
-const port = process.env.CART_SERVER_PORT || '8080';
+const port = process.env.CART_SERVER_PORT || '8081';
 app.listen(port, () => {
     logger.info('Started on port', port);
 });
